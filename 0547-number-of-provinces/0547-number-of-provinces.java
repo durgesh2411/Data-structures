e68325c7ab535b972505cd151908b2cee066671e
Durgesh Kumar Singh <durgesh.vns24@gmail.com>
2025-03-21 22:22:30 +0530

    Time: 6 ms (9.26%), Space: 46.8 MB (89.75%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,60 +1,42 @@
 class Solution {
 
-
+    public void bfs(int start, Set<Integer> vis, List<List<Integer>> adjList){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        vis.add(start);
+        while(!q.isEmpty()){
+                int curr = q.poll();
+                for(int nextVal : adjList.get(curr)){
+                    if(!vis.contains(nextVal)){
+                        q.add(nextVal);
+                        vis.add(nextVal);
+                    }
+                }
+        }
+    }
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length; 
-        DSU dsu = new DSU(n);
-      for (int i = 0; i < n; i++) {
-         for (int j = 0; j<n; j++) { // j==nextNode
-         if(isConnected[i][j] == 1)
-            dsu.union(i, j);
-         }
-      }
-      Set<Integer> count = new HashSet<>(); 
-        for(int i = 0; i < dsu.parent.length; i++) {
-            count.add(dsu.find(dsu.parent[i]));
+        List<List<Integer>> adjList = new ArrayList<>();
+        Set<Integer> vis = new HashSet<>();
+        int n = isConnected.length, m = isConnected[0].length;
+        for(int i=0;i<n;i++){
+            adjList.add(new ArrayList<>());
         }
-        return count.size();
-    }
-
-    public class DSU{
-    private int[] parent;
-    private int[] rank;
-
-    public DSU(int n) {
-        this.parent = new int[n];
-        this.rank = new int[n];
-        for(int i=0;i<n;i++) {
-            parent[i]=i;
-            rank[i]=0;
-        }
-    }
-
-        public int find(int x) {
-            if(parent[x] != x) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    adjList.get(i).add(j);
+                    adjList.get(j).add(i);
+                }
+            }    
         } 
 
-        public void union (int x,int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-
-            if(rootX != rootY){
-            if(rank[rootX] > rank[rootY]){
-                parent[rootY] = rootX;
-            }
-            else if(rank[rootY] > rank[rootX]){
-                parent[rootX] = rootY;
-            }
-            else{
-                parent[rootX] = rootY;
-                rank[rootY]++;
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            if(!vis.contains(i)){
+                bfs(i, vis, adjList);
+                count++;
             }
         }
-        }
+        return count;
     }
 }
-
-
